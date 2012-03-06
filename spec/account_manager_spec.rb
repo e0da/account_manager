30e340e61a69e5e04363bf078f64515ca9c0bcb4
Justin Force <justin.force@gmail.com>
2012-03-05 16:46:06 -0800

    implement password change success

--- spec/account_manager_spec.rb
+++ spec/account_manager_spec.rb
@@ -1,15 +1,106 @@
 require 'spec_helper'
+require 'ladle'
+require 'net-ldap'
 
 module AccountManager
 
-  describe App, type: :request do
+  describe App do
 
-    describe 'GET /' do
+    describe 'GET /', type: :request  do
       it 'redirects to /change_password' do
         visit '/'
         page.current_path.should == '/change_password'
       end
     end
 
+    context 'directory operations' do
+
+      around :each do |example|
+        ladle = start_ladle
+        example.run
+        ladle.stop
+      end
+
+      describe 'sanity' do
+
+        it 'authenticates with known good credentials' do
+          uid = 'aa729'
+          password = 'smada'
+          open_ldap do |ldap|
+            ldap.auth "uid=#{uid},ou=people,dc=example,dc=org", password
+            ldap.bind.should be true
+          end
+        end
+      end
+
+      context 'a user', type: :request do
+        describe 'changes their password' do
+
+          before :each do
+            visit '/change_password'
+          end
+
+
+          context 'a successful attempt' do
+            it 'updates the password' do
+              uid = 'aa729'
+              old_password = 'smada'
+              new_password = 'chickenDinnerPunchFight5'
+
+              open_ldap do |ldap|
+                ldap.auth "uid=#{uid},ou=people,dc=example,dc=org", old_password
+                ldap.bind.should be true
+              end
+
+              fill_in 'Username', with: uid
+              fill_in 'Password', with: old_password
+              fill_in 'New Password', with: new_password
+              fill_in 'Verify New Password', with: new_password
+              check 'agree'
+              click_on 'Change My Password'
+              page.should have_content 'Your password has been changed'
+
+              open_ldap do |ldap|
+                ldap.auth "uid=#{uid},ou=people,dc=example,dc=org", new_password
+                ldap.bind.should be true
+              end
+            end
+
+            it 'activates an inactive account'
+            it 'does not change activation date on an active account'
+          end
+
+          context 'an unsuccessful attempt' do
+            it 'does not update the directory'
+            it 'redirects back to /change_password and reports an error'
+          end
+        end
+
+        describe 'requests a reset token' do
+          it 'deletes any existing reset tokens'
+          it 'creates a new reset token'
+          it 'emails the reset token to the user'
+        end
+
+        describe 'resets their password' do
+          context 'a successful attempt' do
+            it 'changes the password'
+            it 'deletes the reset token'
+          end
+
+          context 'an unsuccessful attempt' do
+            it 'does not update the directory'
+          end
+
+          context 'the token is expired' do
+            it 'informs the user the token is expired'
+            it 'deletes the token'
+            it 'prompts the user to try again'
+          end
+        end
+      end
+
+      context 'an administrator'
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,34 @@ $: << '.'
 require 'account_manager'
 require 'capybara/rspec'
 
+AccountManager::App.environment = :test
+
 Capybara.app = AccountManager::App
+
+def start_ladle
+  ldif = File.expand_path "../../config/test.ldif", __FILE__
+  jar = File.expand_path '../../support/gevirtz_schema/target/gevirtz-schema-1.0-SNAPSHOT.jar', __FILE__
+  opts = {
+    quiet: true,
+    tmpdir: 'tmp',
+    ldif: ldif,
+    additional_classpath: jar,
+    custom_schemas: 'edu.ucsb.education.account.GevirtzSchema'
+  }
+  Ladle::Server.new(opts).start
+end
+
+def read_conf
+  @conf ||= YAML.load_file File.expand_path("../../config/test.yml", __FILE__)
+end
+
+def open_ldap
+  read_conf
+  Net::LDAP.open(
+    host: @conf['host'],
+    port: @conf['port'],
+    base: @conf['base']
+  ) do |ldap|
+    yield ldap
+  end
+end
