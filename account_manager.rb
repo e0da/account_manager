b6fcf065f9032bde42c843f02bb21629f09105b1
Justin Force <justin.force@gmail.com>
2012-03-07 10:24:02 -0800

    Get all tests passing again with change_password using admin

--- account_manager.rb
+++ account_manager.rb
@@ -24,11 +24,15 @@ module AccountManager
 
       set :haml, { :format => :html5 }
       set :sass, Compass.sass_engine_options
+
     end
 
     configure :development do
       register Sinatra::Reloader
     end
+
+    # def initialize
+    # end
 
     helpers do
 
@@ -53,6 +57,14 @@ module AccountManager
         end
       end
 
+      def ldap_open_as_admin
+        ldap_open do |ldap|
+          ldap.auth @conf['bind_dn'] % @conf['admin_username'], @conf['admin_password']
+          ldap.bind
+          yield ldap if block_given?
+        end
+      end
+
       #
       # convenience wrapper for Net::LDAP#search since we do it SO MUCH
       #
@@ -66,8 +78,14 @@ module AccountManager
 
       #
       # crypto methods
-      # TODO move these into a module or something? at least a helper file?
+      # TODO move these into a class or something? at least a helper file?
       #
+
+      # Default hash type is SSHA for production and SHA for test/development
+      #
+      def default_hash_type
+        App.environment == :production ? :ssha : :sha
+      end
 
       # get 16 random hex bytes
       #
@@ -75,37 +93,77 @@ module AccountManager
         16.times.inject('') {|t| t << rand(16).to_s(16)}
       end
 
-      # hash the password using the given salt. If no salt is supplied, use a new
-      # one.
+      # Hash the given password. You can supply a hash type and a salt. If no
+      # hash is supplied, :ssha is used. If not salt is supplied but one is
+      # required, a new salt is generated.
       #
-      def hash_password(password, salt=new_salt)
-        "{SSHA}" + Base64.encode64("#{Digest::SHA1.digest("#{password}#{salt}")}#{salt}").chomp
+      def hash_password(password, opts=nil)
+
+        opts = {} unless opts
+        opts[:type] ||= opts[:salt] ? :ssha : default_hash_type
+        opts[:salt] ||= new_salt if opts[:type] == :ssha
+
+        case opts[:type]
+        when :ssha
+          '{SSHA}'+Base64.encode64(Digest::SHA1.digest(password + opts[:salt]) + opts[:salt]).chomp
+        when :sha
+          Net::LDAP::Password.generate :sha, password
+        else
+          raise "Unsupported password hash type #{type}"
+        end
+      end
+
+      # Check password against SSHA hash
+      #
+      def check_ssha_password(password, original_hash)
+        decoded = Base64.decode64 original_hash.gsub(/^{SSHA}/, '')
+        hash = decoded[0,20]
+        salt = decoded[20,40]
+        hash_password(password, salt: salt) == original_hash
+      end
+
+      # Check password against SHA hash
+      #
+      def check_sha_password(password, original_hash)
+        Net::LDAP::Password.generate(:sha, password) == original_hash
       end
 
       # Check the supplied password against the given hash and return true if they
-      # match, else false.
+      # match, else false. Supported hash types are SSHA and SHA.
       #
-      def check_password(password, ssha)
-        decoded = Base64.decode64(ssha.gsub(/^{SSHA}/, ''))
-        hash = decoded[0,20] # isolate the hash
-        salt = decoded[20,40] # isolate the salt
-        hash_password(password, salt) == ssha
+      def check_password(password, original_hash)
+
+        type = original_hash.match(/{(\S+)}/)[1].downcase.to_sym
+
+        case type
+        when :ssha
+          check_ssha_password(password, original_hash)
+        when :sha
+          check_sha_password(password, original_hash)
+        else
+          raise "Unsupported password hash type #{type}"
+        end
       end
 
+      # In our production environment, you can't bind until your account has
+      # been activated, so we must verify the password without binding. To do
+      # this we just compare hashes.
       #
-      # Return an LDAP-ready hashed password generated from the unhashed
-      # password passed in. In our production environment, we use a SSHA hash.
-      # This isn't supported in net-ldap or the bundled version of Ladle. So we
-      # use SHA for them (handily provided by net-ldap) and roll our own SSHA
-      # for production.
-      #
-      def hashed_password(unhashed_password)
-        if App.environment == :production
-          salt = 20.times.inject('') {|out| out+=%w[0 1 2 3 4 5 6 7 8 9 a b c d e f][rand(16)]}
-          "{SSHA}"+Base64.encode64(Digest::SHA1.digest(unhashed_password+salt)+salt).chomp
-        else
-          Net::LDAP::Password.generate :sha, unhashed_password
+      def verify_password(uid, password)
+        hash = nil
+        ldap_search "(uid=#{uid})" do |entry|
+          hash = entry[:userpassword].first
         end
+        check_password(password, hash)
+      end
+
+      def active?(uid)
+        active = false
+        ldap_search "(uid=#{uid})" do |entry|
+          active = !entry[:ituseagreementacceptdate].first.match(/activation required/)
+        end
+
+        active
       end
 
       def change_password(uid, old_password, new_password)
@@ -113,18 +171,17 @@ module AccountManager
         dn = @conf['bind_dn'] % uid
         timestamp = Time.now.strftime '%Y%m%d%H%M%SZ'
 
-        ldap_open do |ldap|
-          ldap.auth dn, old_password
+        if verify_password uid, old_password
 
-          if ldap.bind
+          ldap_open_as_admin do |ldap|
 
             # ituseagreementacceptdate must come first so that if it quietly
             # fails (as it should if this is an already-activated account) the
             # success of the other transactions still counts
             #
-            ldap.replace_attribute dn, 'ituseagreementacceptdate', timestamp # FIXME this should be conditional
+            ldap.replace_attribute dn, 'ituseagreementacceptdate', timestamp unless active? uid
             ldap.replace_attribute dn, 'passwordchangedate',       timestamp
-            ldap.replace_attribute dn, 'userpassword',             hashed_password(new_password)
+            ldap.replace_attribute dn, 'userpassword',             hash_password(new_password)
           end
         end
       end
