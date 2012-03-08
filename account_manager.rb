22aad96b09ca36426a3295d52110f39ab5d6123a
Justin Force <justin.force@gmail.com>
2012-03-08 10:15:32 -0800

    Move all directory logic into Directory class; handle activation attrs

--- account_manager.rb
+++ account_manager.rb
@@ -45,65 +45,6 @@ module AccountManager
       end
       alias url uri
       alias to uri
-
-      # In our production environment, you can't bind until your account has
-      # been activated, so we must verify the password without binding. To do
-      # this we just compare hashes.
-      #
-      def verify_user_password(uid, password)
-        hash = nil
-        Directory.search "(uid=#{uid})" do |entry|
-          hash = entry[:userpassword].first
-        end
-        Crypto.check_password(password, hash)
-      end
-
-      def set_activation(uid, active=true)
-        Directory.open_as_admin do |ldap|
-          ldap.replace_attribute Directory.bind_dn(uid), 'ituseagreementacceptdate', (active ? timestamp : 'activation required')
-        end
-      end
-      alias activate set_activation
-
-      def deactivate(uid)
-        set_activation uid, false
-      end
-
-      # get a timestamp per request
-      #
-      def timestamp
-        @timestamp ||= Time.now.strftime '%Y%m%d%H%M%SZ'
-      end
-
-      # Verify that the account exists; verify that the username and password
-      # match; activate the account if it isn't active; AS THE USER, set the
-      # password and password change date.
-      #
-      # FIXME the following attributes need to be updated to activate the
-      #       account. Refer to ldapUtil.java:656 for details:
-      #
-      #   - DELETE  "nsroledn: cn=nsmanageddisabledrole,o=education.ucsb.edu"
-      #   - DELETE  "nsaccountlock:"
-      #   - REPLACE "ituseagreementacceptdate: #{timestamp}"
-      #
-      def change_password(uid, old_password, new_password)
-
-        unless Directory.active? uid
-          activate uid 
-          temporary_activation = true
-        end
-
-        dn = Directory.bind_dn uid
-
-        Directory.open_as uid, old_password do |ldap|
-          ldap.replace_attribute dn, 'userpassword', new_password
-        end
-        Directory.open_as_admin do |ldap|
-          ldap.replace_attribute dn, 'passwordchangedate', timestamp
-        end
-      rescue Net::LDAP::LdapError
-        deactivate uid if temporary_activation
-      end
     end
 
     #
@@ -133,9 +74,10 @@ module AccountManager
     end
 
     post '/change_password' do
-      if change_password params[:uid], params[:password], params[:new_password]
+      case Directory.change_password params[:uid], params[:password], params[:new_password]
+      when :success
         flash[:notice] = 'Your password has been changed'
-      else
+      when :failure
         flash[:error] = 'Your password has not been changed'
       end
       redirect to '/change_password'
--- lib/account_manager/directory.rb
+++ lib/account_manager/directory.rb
@@ -1,8 +1,15 @@
 require 'net-ldap'
 
+#
+# TODO documentation
+#
+
 module AccountManager
   class Directory
     class << self
+
+      INACTIVE_VALUE = 'activation required'
+      DISABLED_ROLE = 'cn=nsmanageddisabledrole,o=education.ucsb.edu'
 
       # Read the configuration and cache it. Returns a hash of the
       # configuration. Call it within other static methods conf[:attribute].
@@ -11,13 +18,22 @@ module AccountManager
         @@conf ||= YAML.load_file File.expand_path("#{App.root}/config/#{App.environment}.yml", __FILE__)
       end
 
-      # Wrap Net::LDAP#open, bind as admin, then execute the block in the
-      # context of the Net::LDAP#open block. Returns the return value of the
-      # block.
-      #
+      def open
+        Net::LDAP.open(
+          host: conf['host'],
+          port: conf['port'],
+          base: conf['base']
+        ) do |ldap|
+          yield ldap
+        end
+      end
 
-      def open_as_admin
-        open_as_dn conf['admin_bind_dn'], conf['admin_password'] do |ldap|
+     def open_as_dn(dn, password)
+        open do |ldap|
+          ldap.auth dn, password
+          unless ldap.bind
+            raise Net::LDAP::LdapError
+          end
           yield ldap
         end
       end
@@ -28,23 +44,19 @@ module AccountManager
         end
       end
 
-      def open_as_dn(dn, password)
-        Net::LDAP.open(
-          host: conf['host'],
-          port: conf['port'],
-          base: conf['base']
-        ) do |ldap|
-          ldap.auth dn, password
-          unless ldap.bind
-            raise Net::LDAP::LdapError
-          end
+      # Wrap Net::LDAP#open, bind as admin, then execute the block in the
+      # context of the Net::LDAP#open block. Returns the return value of the
+      # block.
+      #
+      def open_as_admin
+        open_as_dn conf['admin_bind_dn'], conf['admin_password'] do |ldap|
           yield ldap
         end
       end
 
-      # Wrap open {|ldap| ldap.search(filter: filter)} and perform searches while bound as
-      # admin, then execute the block (if given) in the context of the
-      # Net::LDAP#search. Returns the list of entries.
+      # Wrap open_as_admin {|ldap| ldap.search(filter: filter)} and perform
+      # searches while bound as admin, then execute the block (if given) in the
+      # context of the Net::LDAP#search. Returns the list of entries.
       #
       def search(filter)
         open_as_admin do |ldap|
@@ -61,16 +73,76 @@ module AccountManager
         conf['bind_dn'] % uid
       end
 
+      # Verify that the account exists; verify that the username and password
+      # match; activate the account if it isn't active; AS THE USER, set the
+      # password and password change date.
+      #
+      #
+      #
+      def change_password(uid, old_password, new_password)
+
+        timestamp = Time.now.strftime '%Y%m%d%H%M%SZ'
+
+        unless activated? uid
+          activate uid, timestamp
+          temporary_activation = true
+        end
+
+        if bind uid, old_password
+          open_as uid, old_password do |ldap|
+            dn = bind_dn(uid)
+            ldap.replace_attribute dn, :userpassword, Crypto.hash_password(new_password)
+            ldap.replace_attribute dn, :passwordchangedate, timestamp
+          end
+          :success
+        else
+          deactivate uid if temporary_activation
+          :failure
+        end
+      end
+
       # Return true if the user identified by uid has a timestamp in the
       # ituseagreementacceptdate attribute.
       #
-      def active?(uid)
-        active = false
-        Directory.search "(uid=#{uid})" do |entry|
-          active = !entry[:ituseagreementacceptdate].first.match(/activation required/)
+      def activated?(uid)
+        activated = false
+        search "(uid=#{uid})" do |entry|
+          activated = !entry[:ituseagreementacceptdate].first.match(/#{INACTIVE_VALUE}/)
         end
 
-        active
+        activated
+      end
+
+      def activate(uid, timestamp)
+        open_as_admin do |ldap|
+          operations = [
+            [:replace, :ituseagreementacceptdate, timestamp],
+            [:delete, :nsroledn, 'cn=nsmanageddisabledrole,o=education.ucsb.edu'],
+            [:delete, :nsaccountlock, nil]
+          ]
+          ldap.modify dn: bind_dn(uid), operations: operations
+        end
+      end
+
+      def deactivate(uid)
+        open_as_admin do |ldap|
+          operations = [
+            [:replace, :ituseagreementacceptdate, INACTIVE_VALUE],
+            [:delete, :nsroledn, DISABLED_ROLE],
+            [:delete, :nsaccountlock, true]
+          ]
+          ldap.modify dn: bind_dn(uid), operations: operations
+        end
+      end
+
+      def bind(uid, password)
+        bound = false
+        open do |ldap|
+          ldap.auth bind_dn(uid), password
+          bound = ldap.bind
+        end
+
+        bound
       end
     end
   end
