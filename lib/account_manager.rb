1a30d2d174dcc1f1d0aa3ea3ed9f5869ec063bbc
Justin Force <justin.force@gmail.com>
2012-05-04 10:50:53 -0700

    rename weak_password.rb to password_strength.rb

--- lib/account_manager.rb
+++ lib/account_manager.rb
@@ -1,4 +1,4 @@
 require 'account_manager/app'
 require 'account_manager/directory'
 require 'account_manager/models'
-require 'account_manager/weak_password.rb'
+require 'account_manager/password_strength.rb'
