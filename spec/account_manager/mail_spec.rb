a80c5a17566a0e5b366e1a6ceed82d11713b5228
Justin Force <justin.force@gmail.com>
2012-05-18 10:43:57 -0700

    Oops. Stub Net::SMTP#start in mail_spec.rb

--- spec/account_manager/mail_spec.rb
+++ spec/account_manager/mail_spec.rb
@@ -15,6 +15,7 @@ module AccountManager
       end
 
       it 'returns success if the mail operation worked' do
+        Net::SMTP.stub start: true
         Mail.reset(nil, @token).should be :success
       end
 
